0d2136e231b927a524b97aad9ba400301bdc64ff
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-10-17 20:52:05 -0500

    changed prompt questions to have validation and added functions to produce badges for licenst

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-// this requires/calls inquirer
+// modules for app
 const inquirer = require('inquirer');
-// this requires the generateMarkdown in ifle
 const generateMarkdown = require('./utils/generateMarkdown.js')
-// this requires/calls file system
+const licenseBadge = require('./utils/generateMarkdown.js')
 const fs = require('fs')
+
+
+
 // array of questions for user
 const questions = [
     {
         type: 'input',
         name: 'Title',
         message: 'What is the title of your project? (REQUIRED)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+              console.log('Please enter your project title name!');
+              return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'Description',
         message: 'Please provide a description of your project. (REQUIRED)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+              console.log('Please enter a description of your project!');
+              return false;
+            }
+        }
     },
     {
         type: 'checkbox',
@@ -26,23 +44,47 @@ const questions = [
     {
         type: 'input',
         name: 'Installation',
-        message: 'Please provide required installations for this application.',
+        message: 'Please provide required installations for this application. (REQUIRED)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+              console.log('Please provide installation info for this application!');
+              return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'Usage',
-        message: 'Please provide the usage instructions.',
+        message: 'Please provide the usage instructions. (REQUIRED)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+              console.log('Please provide instructions on how to use the application!');
+              return false;
+            }
+        }
     },
     {
         type: 'list',
         name: 'License',
-        choices: ['Apache License 2.0', ' MIT License', 'Eclipse Public License 2.0', 'Mozilla Public License 2.0'],
-        message: 'Select the type of license.',
+        choices: ['Apache License 2.0', ' MIT License', 'Eclipse Public License 2.0', 'Mozilla Public License 2.0', 'GNU gpl'],
+        message: 'Select the license this application is covered under, if no license press enter.',
     },
     {
         type: 'input',
         name: 'Contributors',
-        message: 'Please provide names of contributing members to your application.'
+        message: 'Please provide names of contributing members to your application. (REQUIRED)',
+        validate: contributorsInput => {
+            if (contributorsInput) {
+                return true;
+            } else {
+              console.log('Please provide names of contributing members!');
+              return false;
+            }
+        }
     },
     {
         type: 'input',
@@ -52,12 +94,28 @@ const questions = [
     {
         type: 'input',
         name: 'Github',
-        message: 'What is your GitHub username?',
+        message: 'What is your GitHub username? (REQUIRED)',
+        validate: gitHubInput => {
+            if (gitHubInput) {
+                return true;
+            } else {
+              console.log('Please provide your GitHub username!');
+              return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'Email',
-        message: 'Please provide your email for inquiries regarding your application.',
+        message: 'Please provide your email for inquiries regarding your application.(REQUIRED)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+              console.log('Please provide your email!');
+              return false;
+            }
+        }
     }
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,69 @@
+// function to return licensure badge
+// returns empty string if no license
+function licenseBadge(license) {
+  if (license == "MIT") {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-blue)`
+  } else if (license == "Apache 2.0") {
+    return `![License](https://img.shields.io/badge/License-Apache%202.0-blue)`
+  } else if (license == "GNU GPL v3") {
+    return `![License: GPL v3](https://img.shields.io/badge/License-GNU%20GPL%20v3-blue)`
+  } else if (license == "Mozilla Public License 2.0") {
+    return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-blue)`
+  } else if (license == "Eclipse Public License v2.0") {
+    return `![License: EPL v2.0](https://img.shields.io/badge/License-EPL%20v2.0-blue)`
+  } else if (license == "No License") {
+    return ''
+  }
+};
+
+// function to return license link
+// Returns empty string if no license
+function licenseLink(license) {
+  if (license == "MIT") {
+    return `(https://opensource.org/licenses/MIT)`
+  } else if (license == "Apache 2.0") {
+    return `(https://opensource.org/licenses/Apache-2.0)`
+  } else if (license == "GNU GPL v3") {
+    return `(https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (license == "Mozilla Public License 2.0") {
+    return `(https://opensource.org/licenses/MPL-2.0)`
+  } else if (license == "Eclipse Public License v2.0") {
+    return `(https://www.eclipse.org/legal/epl-2.0/)`
+  } else if (license == "No License") {
+    return ''
+  }
+};
+
+
+
+function licenseSection(license) {
+  if (license !== '') {
+    return `## License
+    
+  This project is licensed under [${license}]` + licenseLink(license)
+  }
+  return ''
+};
+
+function licenseTable(license) {
+  if(license !== '') {
+    return `* [License](#license)`
+  }
+  return ''
+};
+
+
+
 // function that creates/generates markdown for READme
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${licenseBadge(data.License)}
 
   ## Table of Contents
   * [Description](#Description)
   * [Installation](#Installation-Instructions)
   * [Usage Instructions](#Usage-Instructions)
-  * [License](#License)
+  * ${licenseTable(data.license)}
   * [Contributing Members](#Contributing-Members)
   * [Testing](#Testing)    
   * [Questions/Inquiries](#Questions/Inquiries)
@@ -20,16 +77,13 @@ function generateMarkdown(data) {
   ## Usage Instructions
   ${data.Usage}
 
-  ## License
-  ${data.License}
+  ${licenseSection(data.license)}
 
   ## Contributing Members
   ${data.Contributors}
 
   ## Testing 
    ${data.Test}
-
-  ## Questions/Inquiries 
 
   ### GitHub Profile
   [GitHub Profile](http://github.com/${data.Github})
